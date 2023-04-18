0e68ca5c4ff02e9270966a84f26aae82f6671271
unknown <hizkiagenius@gmail.com>
2023-04-18 10:28:34 +0700

    added one endpoint, just select all data for now

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const { Pool } = require('pg');
+
+const pool = new Pool({ //later, secure this by adding env variable or something
+  user: 'postgres',
+  host: 'localhost',
+  database: 'if4040',
+  password: 'postgres',
+  port: 5432
+});
+
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -13,6 +24,16 @@ app.use(
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+
+app.get('/precomputed_view', (request, response) => {
+  pool.query('SELECT * FROM social_medua', (err, results) =>{
+    if (err){
+      throw err;
+    }
+    response.status(200).json(results.rows);
+  })
+})
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
